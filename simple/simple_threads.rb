ed04241fa618ff4816089d95f33e27637ff474f2
John Mazzitelli <mazz@redhat.com>
2016-02-18 16:16:43 -0500

    threads

--- simple/simple_threads.rb
+++ simple/simple_threads.rb
@@ -12,3 +12,16 @@ threads = sites.map do |site_to_fetch|
 end
 
 threads.each { |thr| thr.join }
+
+# Thread.value returns the last statement evaluated by the thread
+
+t2 = Thread.new() do
+  3.times do |v|
+    p "#{v}..."
+    sleep v
+  end
+  'done'
+end
+
+# Thread.value waits for the thread to end via join then returns the last value evaluated by thread
+print "Thread last value=#{t2.value}"

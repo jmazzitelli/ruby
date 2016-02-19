8f971882c5a61588c2cb0f009e62c79e6afe8e8b
John Mazzitelli <mazz@redhat.com>
2016-02-19 15:24:36 -0500

    modules

--- simple/simple_module.rb
+++ simple/simple_module.rb
@@ -4,3 +4,27 @@ module MySimpleModule
     puts MYVAR, param1 , param2
   end
 end
+
+module OuterModule
+  module InnerModule
+    class MyClass
+      attr_accessor(:name)
+    end
+  end
+
+  class Parent < InnerModule::MyClass
+    attr_accessor(:title)
+    def initialize(name, title)
+      @name = name
+      @title = title
+    end
+  end
+end
+
+myc = OuterModule::InnerModule::MyClass.new
+myc.name = "boo"
+p myc.name
+
+myp = OuterModule::Parent.new("myname", "mytitle")
+p myp.name
+p myp.title
